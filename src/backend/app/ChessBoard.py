3f9f71c7a73077d2b991f4aaf1e3f4f8b47d711d
SGF-14 <a.h.sgfsc@gmail.com>
2024-04-28 00:35:27 +0300

    breakdown match process, create FrameManagement to fox import circular, mange flask app with frontend, temp update locations of squares

---
+++ src/backend/app/ChessBoard.py
@@ -0,0 +1,33 @@
+# ChessBoard.py
+import chess
+import chess.svg
+import cairosvg
+import io
+
+board = chess.Board()
+
+def get_board():
+    global board
+    return board
+
+def reset_board():
+    global board
+    board.reset()
+
+def get_board_png(board: chess.Board, size=900):
+    print(board)
+    svg_content = chess.svg.board(board=board,orientation=chess.BLACK, size=size)
+    css_styles = """<style>
+    .square.light { fill: #ebedd1; } 
+    .square.dark  { fill: #739453; } 
+    .white.piece { fill: #f8f8f9; stroke: #000; } 
+    .black.piece { fill: #5c5856; stroke: #fff; } 
+    </style>"""
+    
+    insert_position = svg_content.find('>') + 1
+    svg_with_css = svg_content[:insert_position] + css_styles + svg_content[insert_position:]
+
+    png_bytes = cairosvg.svg2png(bytestring=svg_content.encode('utf-8'))
+    return png_bytes
+    # svg2png(bytestring=svg_with_css.encode('utf-8'), write_to='styled_chess_board.png')
+    
--- src/backend/app/api.py
+++ src/backend/app/api.py
@@ -1,6 +1,7 @@
 # api.py
-from flask import Blueprint, request, jsonify
-from .match import start_chess_match
+from flask import Blueprint, request, jsonify, send_file, make_response
+from .ChessBoard import get_board, get_board_png
+from .match import start_chess_match, end_turn, complete_chess_match
 
 api_blueprint = Blueprint('api', __name__)
 
@@ -11,10 +12,39 @@ def start_match():
     if not username:
         return jsonify({'error': 'Username is required'}), 400
     try:
-        result = start_chess_match(username)  # Ensure this function returns some status
+        result = start_chess_match(username)  
         if result == "Match Started":
             return jsonify({'message': 'Match started successfully'}), 200
         else:
-            return jsonify({'error': 'A match is currently active. Please wait.'}), 403  # or another appropriate status code
+            return jsonify({'error': 'A match is currently active. Please wait.'}), 403  
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+@api_blueprint.route('/end_turn', methods=['POST'])
+def end_turn_endpoint():
+    _, error = end_turn()
+    if error is None:
+        return jsonify({'message': 'Turn processed successfully'}), 200
+    else:
+        return jsonify({'error': error}), 400
+
+
+@api_blueprint.route('/complete_chess_match', methods=['POST'])
+def complete_chess_match_endpoint():
+    data = request.get_json()
+    match_id = data.get('match_id')
+    try:
+        result = complete_chess_match(match_id)
+        if result == "Match completed":
+            return jsonify({'status': 'Match completed successfully'}), 200
+        else:
+            return jsonify({'status': 'Failed to complete the match', 'error': result}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@api_blueprint.route('/get_board_image', methods=['GET'])
+def get_board_image():
+    try:
+        return send_file('../styled_chess_board.png', mimetype='image/png') 
+    except Exception as e:
+        return str(e), 500
